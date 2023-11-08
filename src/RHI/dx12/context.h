a0b80a9a5c8a174ca70b43d6386f8da6992fbddb
xiongyaya <1157988949@qq.com>
2023-11-09 00:03:08 +0800

    添加dx12

---
+++ src/RHI/dx12/context.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include"core/util/singleton.h"
+
+class DX12Context : public Singleton< DX12Context>
+{
+public:
+	void initialize() {
+
+	}
+	void tick() {
+
+	}
+	void finalize() {
+
+	}
+};
